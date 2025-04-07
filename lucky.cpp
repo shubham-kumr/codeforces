9bd5bbe1e708756163b8a697329358262e614f39
Shubham Kumar <154588634+shubham-kumr@users.noreply.github.com>
2025-04-07 20:34:53 +0530

    lucky

---
+++ lucky.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string ticket;
+        cin >> ticket;
+
+        int sum1 = ticket[0] - '0' + ticket[1] - '0' + ticket[2] - '0';
+        int sum2 = ticket[3] - '0' + ticket[4] - '0' + ticket[5] - '0';
+
+        if (sum1 == sum2) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}   
