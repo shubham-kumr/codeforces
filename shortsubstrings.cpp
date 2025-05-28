b586414a5ac9d4615b237d440ef99ceb02a98eb2
Shubham Kumar <154588634+shubham-kumr@users.noreply.github.com>
2025-05-28 21:27:41 +0530

    Short Substrings Problems

---
+++ shortsubstrings.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string b;
+        cin >> b;
+        string a;
+        a += b[0];
+        for (size_t i = 1; i < b.size(); i += 2) {
+            a += b[i];
+        }
+        cout << a << endl;
+    }
+    return 0;
+}
