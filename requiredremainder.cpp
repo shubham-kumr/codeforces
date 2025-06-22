ced439f886fc4ab40bcdbed6a723bbb151eb6e61
Shubham Kumar <154588634+shubham-kumr@users.noreply.github.com>
2025-06-22 21:19:43 +0530

    Required Remainder

---
+++ requiredremainder.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long x, y, n;
+        cin >> x >> y >> n;
+        long long k = ((n - y) / x) * x + y;
+        cout << k << '\n';
+    }
+    return 0;
+}
